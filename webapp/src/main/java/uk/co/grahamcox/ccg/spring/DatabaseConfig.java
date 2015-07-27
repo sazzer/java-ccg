8146461a5835bc960195d142338c197a0e9f2d9d
Graham Cox <graham@grahamcox.co.uk>
2015-07-27 22:10:00 +0100

    Added in Liquibase for setting up the database

--- webapp/src/main/java/uk/co/grahamcox/ccg/spring/DatabaseConfig.java
+++ webapp/src/main/java/uk/co/grahamcox/ccg/spring/DatabaseConfig.java
@@ -1,5 +1,11 @@
 package uk.co.grahamcox.ccg.spring;
 
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.ClassLoaderResourceAccessor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +15,7 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 
 import javax.sql.DataSource;
+import java.sql.SQLException;
 
 /**
  * Spring Configuration for connecting to the database
@@ -20,14 +27,23 @@ public class DatabaseConfig {
     /**
      * Construct an in-memory data source if needed
      * @return the data source
+     * @throws SQLException if an error occurs connecting to the database to ingest the schema
+     * @throws LiquibaseException if an error occurs ingesting the schema
      */
     @Bean(name = "dataSource")
     @Profile({"dev", "test"})
-    public DataSource inMemoryDataSource() {
-        EmbeddedDatabase database = new EmbeddedDatabaseBuilder()
+    public DataSource inMemoryDataSource() throws SQLException, LiquibaseException {
+        EmbeddedDatabase dataSource = new EmbeddedDatabaseBuilder()
             .build();
 
-        LOG.info("Created in-memory database: {}", database);
-        return database;
+        Database database = DatabaseFactory.getInstance()
+            .findCorrectDatabaseImplementation(new JdbcConnection(dataSource.getConnection()));
+        Liquibase liquibase = new Liquibase("uk/co/grahamcox/ccg/database/changelog.master.xml",
+            new ClassLoaderResourceAccessor(),
+            database);
+        liquibase.updateTestingRollback(null);
+
+        LOG.info("Created in-memory database: {}", dataSource);
+        return dataSource;
     }
 }
